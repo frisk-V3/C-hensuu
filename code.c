54c54ab0a0cc95431d6c84dca2b7d3ef8c670bf6
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-05-10 09:00:34 +0900

    Add files via upload

---
+++ code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 16;
+    float height = 170.5;
+    char grade = 'A';
+
+    printf("年齢: %d\n", age);
+    printf("身長: %.1f\n", height);
+    printf("成績: %c\n", grade);
+    return 0;
+}
